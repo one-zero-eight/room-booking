da7dd7ba558f85751da9b91584f5e85552208b42
Artem Bulgakov <ArtemSBulgakov@ya.ru>
2025-10-15 03:59:47 +0300

    fix: fastapi does not support 'type ... = ...'

--- src/api/dependencies.py
+++ src/api/dependencies.py
@@ -7,4 +7,4 @@ from fastapi import Depends
 from src.modules.auth.dependencies import verify_user
 from src.modules.tokens.repository import UserTokenData
 
-type VerifiedDep = Annotated[UserTokenData, Depends(verify_user)]
+VerifiedDep = Annotated[UserTokenData, Depends(verify_user)]
