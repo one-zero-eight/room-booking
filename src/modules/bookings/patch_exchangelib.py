17681770deaaf296608d711a27890d6c0c605d64
Ruslan Bel'kov <ruslan.belckov@yandex.ru>
2026-02-11 20:05:33 +0300

    chore: avoid extra request to get timezones from EWS

--- src/modules/bookings/patch_exchangelib.py
+++ src/modules/bookings/patch_exchangelib.py
@@ -1,3 +1,5 @@
+import datetime
+
 import exchangelib
 import pycurl
 import requests
@@ -45,4 +47,55 @@ def raw_session(cls, prefix, oauth2_client=None, oauth2_session_params=None, oau
     return session
 
 
+def get_free_busy_info(self, accounts, start, end, merged_free_busy_interval=30, requested_view="DetailedMerged"):
+    """Return free/busy information for a list of accounts.
+
+    :param accounts: A list of (account, attendee_type, exclude_conflicts) tuples, where account is either an
+        Account object or a string, attendee_type is a MailboxData.attendee_type choice, and exclude_conflicts is a
+        boolean.
+    :param start: The start datetime of the request
+    :param end: The end datetime of the request
+    :param merged_free_busy_interval: The interval, in minutes, of merged free/busy information (Default value = 30)
+    :param requested_view: The type of information returned. Possible values are defined in the
+        FreeBusyViewOptions.requested_view choices. (Default value = 'DetailedMerged')
+
+    :return: A generator of FreeBusyView objects
+    """
+    from exchangelib.account import Account
+    from exchangelib.properties import (
+        DaylightTime,
+        FreeBusyViewOptions,
+        MailboxData,
+        StandardTime,
+        TimeWindow,
+        TimeZone,
+    )
+    from exchangelib.services.get_user_availability import GetUserAvailability
+
+    timezone = TimeZone(
+        bias=-180,
+        standard_time=StandardTime(bias=0, time=datetime.time(0, 0), occurrence=1, iso_month=1, weekday="Monday"),
+        daylight_time=DaylightTime(bias=0, time=datetime.time(0, 0), occurrence=5, iso_month=12, weekday="Sunday"),
+    )
+
+    return GetUserAvailability(self).call(
+        tzinfo=start.tzinfo,
+        mailbox_data=[
+            MailboxData(
+                email=account.primary_smtp_address if isinstance(account, Account) else account,
+                attendee_type=attendee_type,
+                exclude_conflicts=exclude_conflicts,
+            )
+            for account, attendee_type, exclude_conflicts in accounts
+        ],
+        timezone=timezone,
+        free_busy_view_options=FreeBusyViewOptions(
+            time_window=TimeWindow(start=start, end=end),
+            merged_free_busy_interval=merged_free_busy_interval,
+            requested_view=requested_view,
+        ),
+    )
+
+
 exchangelib.protocol.BaseProtocol.raw_session = raw_session
+exchangelib.protocol.Protocol.get_free_busy_info = get_free_busy_info
